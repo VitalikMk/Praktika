6ebd49276d947312c685a99663194c8cf347a1d1
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2023-12-12 13:15:19 +0200

    mod

---
+++ Praktika/BusStop.cs
@@ -0,0 +1,29 @@
+﻿namespace Praktika
+{
+    class BusStop
+    {
+        private Minibus minibus;
+        private int standingCapacity;
+        private int sittingCapacity;
+
+        public BusStop(Minibus minibus, int standingCapacity, int sittingCapacity)
+        {
+            this.minibus = minibus;
+            this.standingCapacity = standingCapacity;
+            this.sittingCapacity = sittingCapacity;
+        }
+
+        // Метод для обслуговування зупинки
+        public void ServeStop()
+        {
+            Random random = new Random();
+            int standingPassengers = random.Next(1, standingCapacity + 1);
+            int sittingPassengers = random.Next(1, sittingCapacity + 1);
+
+            Console.WriteLine($"Зупинка. Стоячих пасажирiв: {standingPassengers}, сидячих пасажирiв: {sittingPassengers}");
+
+            minibus.Embark(sittingPassengers);
+            minibus.Disembark(sittingPassengers);
+        }
+    }
+}
---
+++ Praktika/Minibus.cs
@@ -0,0 +1,45 @@
+﻿namespace Praktika
+{
+    class Minibus
+    {
+        private object locker = new object(); // Об'єкт блокування для потокобезпечності
+        private int availableSeats; // Кількість вільних місць
+        private int standingPassengers; // Кількість пасажирів, що стоять
+
+        public Minibus(int totalSeats, int standingSeats)
+        {
+            availableSeats = totalSeats;
+            standingPassengers = standingSeats;
+        }
+
+        // Метод для сідання пасажирів
+        public void Embark(int sittingPassengers)
+        {
+            lock (locker)
+            {
+                int sittingToSit = Math.Min(sittingPassengers, availableSeats);
+                int standingToSit = Math.Min(sittingPassengers - sittingToSit, standingPassengers);
+
+                availableSeats -= sittingToSit;
+                standingPassengers -= standingToSit;
+
+                Console.WriteLine($"Сiдає {sittingToSit} пасажирiв на сидячi мiсця та {standingToSit} на стоячi мiсця.");
+                Console.WriteLine($"Вiльних сидячих мiсць: {availableSeats}, вiльних стоячих мiсць: {standingPassengers}");
+            }
+        }
+
+        // Метод для висадки пасажирів
+        public void Disembark(int sittingPassengers)
+        {
+            lock (locker)
+            {
+                int sittingToStand = Math.Min(sittingPassengers, availableSeats);
+
+                availableSeats += sittingToStand;
+
+                Console.WriteLine($"Висаджуються {sittingToStand} пасажирiв з сидячих мiсць.");
+                Console.WriteLine($"Вiльних сидячих мiсць: {availableSeats}, вiльних стоячих мiсць: {standingPassengers}");
+            }
+        }
+    }
+}
